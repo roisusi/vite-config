a7c7bd1741b8548b29c21390008e97642c7e2b0d
Roi Susi <65774200+roisusi@users.noreply.github.com>
2025-03-06 10:35:41 +0200

    some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
 import './App.css';
-import { CSSProperties } from 'react';
+import { FullPageAnimatedSvg } from './FullPageAnimatedSvg.tsx';
 
 const App = () => {
-	let a;
-	type myType = number;
-
-	type roi = { x: number };
-
-	const ab = (hello: string): string => {
-		console.log(hello);
-		return 'Hello World';
-	};
-
-	const ahhh: roi = 'd';
-
-	if (a === undefined) {
-		console.log('s');
-	}
-
-	const appf: CSSProperties = {
-		// eslint-disable-next-line no-restricted-syntax
-		margin: '10px',
-	};
-	// bali TODO hello
 	return (
 		<>
-			<p style={appf}></p>
-			{a}
-			{ahhh}
-			{ab('Hello World')}
+			<FullPageAnimatedSvg />
 		</>
 	);
 };
