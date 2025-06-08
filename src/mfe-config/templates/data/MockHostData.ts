4fd1331779d4da595ce2f8dbf7de5057bf204736
Roi Susi <65774200+roisusi@users.noreply.github.com>
2025-06-08 15:34:34 +0300

    mock data

---
+++ src/mfe-config/templates/data/MockHostData.ts
@@ -0,0 +1,13 @@
+import { HostShellData } from '@umtb/shared-ui-utils';
+
+export const MockHostData: HostShellData = {
+	_401Message: {
+		accountResourceId: '59755c14-d677-4288-9646-cf99e57e0506',
+		accountName: '',
+		accountId: '',
+		dd: '123',
+	},
+	userId: '1234567890',
+	customerId: '123456789',
+	userIdNumber: '',
+};
